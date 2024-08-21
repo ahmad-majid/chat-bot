b9f3662112dde1fb4db4638ad0b12b032acfed91
Ahmad Majid <130081012+ahmad-majid@users.noreply.github.com>
2024-08-21 14:18:03 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,17 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-icons']
+    }
+  },
   server: {
     port: 3000,
     proxy: {
       "/api": {
         target: "http://localhost:5000",
-    
       },
+    },
   },
-}});
+});
